7e8e12c049b29f25f4fee317b01e171d1c1a04fb
AntonDePaz <pazjeck14@gmail.com>
2022-01-21 04:12:47 -0800

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ const App = () => {
  
   return (
    <div className='app'>
-     <h2 className='app-header'>Hello Todo List</h2>
-     <p>The most simple and amazing todo-list app</p>
+     <div className='app-header'>
+        <h2 >Hello Todo List</h2>
+        <p>The most simple and amazing todo-list app</p>
+     </div>
    </div>
   );
 };
