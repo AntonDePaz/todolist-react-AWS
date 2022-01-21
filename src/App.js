00a9e390d1e1658500556c4ececcd4932f604285
AntonDePaz <pazjeck14@gmail.com>
2022-01-21 04:30:40 -0800

    Create a css style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
 const App = () => {
+
+  const paraStyle = {
+    "marginBottom" : '15px',
+    'color' : 'red'
+  };
  
   return (
    <div className='app'>
      <div className='app-header'>
-        <h2 >Hello Todo List</h2>
-        <p>The most simple and amazing todo-list app</p>
+        <h2 >My Amazing Todo List App</h2>
+        <p style={paraStyle} >The most simple and amazing todo-list app</p>
+        <ul>
+            <li className='task-item'>Follow Edukasyun ph on Facebook</li>
+            <li className='task-item'>Follow Edukasyun ph on Facebook</li>
+            <li className='task-item'>Follow Edukasyun ph on Facebook</li>
+        </ul>
      </div>
    </div>
   );
