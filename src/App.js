8baec811315ebb2d6b6e7e5d8e308d695c8c5b9f
AntonDePaz <pazjeck14@gmail.com>
2022-01-21 04:44:53 -0800

    Add Function in Task List

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ const App = () => {
     "marginBottom" : '15px',
     'color' : 'red'
   };
+
+  function Tasklist(){
+      return (
+        <ul>
+          <li className='task-item'>Follow Edukasyun ph on Facebook</li>
+          <li className='task-item'>Follow Edukasyun ph on Facebook</li>
+          <li className='task-item'>Follow Edukasyun ph on Facebook</li>
+        </ul>
+      )
+  }
+ 
  
   return (
    <div className='app'>
      <div className='app-header'>
         <h2 >My Amazing Todo List App</h2>
         <p style={paraStyle} >The most simple and amazing todo-list app</p>
-        <ul>
-            <li className='task-item'>Follow Edukasyun ph on Facebook</li>
-            <li className='task-item'>Follow Edukasyun ph on Facebook</li>
-            <li className='task-item'>Follow Edukasyun ph on Facebook</li>
-        </ul>
+           <Tasklist />
      </div>
    </div>
   );
